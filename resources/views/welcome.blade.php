33b0f8369fdaebf148789bcfe122056005cc70d0
Matadamas Zaragoza Julio Cesar <julio.matadamas@dish.com.mx>
2021-01-13 14:32:12 -0600

    se centra el logo en la pagina de inicio.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container centrar">
     <img src="{{ asset('img/brand-transparent.png') }}" alt="">
 </div>
 @endsection
