7e3d54c90ca0184043307ddb4d5ada5eae0704ec
Julio Cesar Matadamas Zaragoza <julius3877@gmail.com>
2021-01-13 20:31:27 -0600

    se agrega la libreria 'animate.css'.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,14 @@
 @section('content')
 <div class="container centrar">
 	<div class="container-logo">
-		<img src="{{ asset('img/brand-transparent.png') }}" alt="">
+		<img src="{{ asset('img/brand-transparent.png') }}" class="img-fluid" alt="">
 		<h1>Point Of Sales System X</h1>
+		<hr>
+		<a href="{{ route('login') }}" >
+			<h2>
+				<i class="fas fa-sign-in-alt"></i>
+			</h2>
+		</a>
 	</div>
 </div>
 @endsection
@@ -13,6 +19,12 @@
     <style type="text/css">
     	.container-logo {
     		text-align: center;
+    	}
+    	.container-logo a {
+    		color: #000;
+    	}
+    	.container-logo a:hover {
+    		color:  #333;
     	}
     </style>
 @endpush
